109d76cfae6f7b627429154fe6f8ad4fed0939f1
Sherzod Nimatullo <shermanjones99@gmail.com>
2019-08-09 15:50:45 -0400

    first commit

---
+++ Hover JS Project/main.js
@@ -0,0 +1,36 @@
+let clearButton = document.querySelector('button');
+clearButton.addEventListener('click', (e) => {
+  clearScreen();
+});
+
+function populate() {
+  let newDiv;
+  let container = document.getElementById('container');
+  for (let i = 1; i < 257; i++) {
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'colorchanger');
+    newDiv.style.height = "20px";
+    container.appendChild(newDiv);
+  }
+  addEvents();
+}
+
+function addEvents() {
+  const divs = document.querySelectorAll('#colorchanger');
+
+  divs.forEach((colorchanger) => {
+    colorchanger.addEventListener('mouseover' , (e) => {
+      colorchanger.style.backgroundColor = "red";
+    })
+  })
+}
+
+function clearScreen() {
+  const divs = document.querySelectorAll('#colorchanger');
+
+  divs.forEach((colorchanger) => {
+    colorchanger.style.backgroundColor = "white";
+  })
+}
+
+document.onload = populate();
