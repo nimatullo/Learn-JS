8e1b3bd7e48c83bf662a4eea3da117f5685e9677
Sherzod Nimatullo <shermanjones99@gmail.com>
2019-10-22 15:14:35 -0400

    js

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,9 +1,39 @@
-buttons = document.querySelectorAll('button');
-textfield = document.querySelector('.screen');
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        number = document.createElement('p');
-        number.textContent = button.textContent;
-        textfield.appendChild(number);
+function assignPrintEvent() {
+    buttons = document.querySelectorAll('.numbers');
+    textfield = document.querySelector('.screen');
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            screenContent = document.querySelector('.screen-content');
+            screenContent.textContent += button.textContent;
+        });
     });
-});
+}
+
+function assignOperationEvent() {
+    let firstNumber;
+    operations = document.querySelectorAll('.operation');
+    operations.forEach(button => {
+        button.addEventListener('click', function () {
+            firstNumber = parseInt(screenContent.textContent, 10);
+            operation = button.id;
+            screenContent.textContent = '';
+        });
+    });
+}
+
+function equals() {
+    equalsButton = document.querySelector('.equals');
+    equalsButton.addEventListener('click', function () {
+        let secondNumber = parseInt(screenContent.textContent, 10);
+        console.log(operation);
+        if (operation == 'add') {
+            answer = firstNumber + secondNumber;
+        }
+
+        screenContent.textContent = answer;
+    });
+}
+
+assignPrintEvent();
+assignOperationEvent();
+equals();
